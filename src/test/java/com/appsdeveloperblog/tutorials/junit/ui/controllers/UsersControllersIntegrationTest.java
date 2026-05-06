6c98e638e0a90e6998d143481250bc3e8704fd1b
parom <E.Parominskiy@isida.by>
2026-05-06 15:31:18 +0300

    93. @SpringBootTest WebEnvironment MOCK

---
+++ src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllersIntegrationTest.java
@@ -0,0 +1,12 @@
+package com.appsdeveloperblog.tutorials.junit.ui.controllers;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest // позволяет использовать все слои spring application
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK) // используется по умолчанию (не поднимает тестовый сервер)
+
+public class UsersControllersIntegrationTest {
+
+
+
+}
