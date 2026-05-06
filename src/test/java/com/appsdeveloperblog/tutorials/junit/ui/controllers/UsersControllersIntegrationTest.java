1b2025e7207f84c26134bb4d8cea1a5e6318a390
parom <E.Parominskiy@isida.by>
2026-05-06 17:37:35 +0300

    98. TestRestTemplate - Prepare & Perform HTTP Post Request

--- src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllersIntegrationTest.java
+++ src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllersIntegrationTest.java
@@ -1,14 +1,20 @@
 package com.appsdeveloperblog.tutorials.junit.ui.controllers;
 
+import com.appsdeveloperblog.tutorials.junit.ui.response.UserRest;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.*;
 import org.springframework.test.context.TestPropertySource;
+
+import java.util.Arrays;
 
 //@SpringBootTest // позволяет использовать все слои spring application
 //@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK) // используется по умолчанию (не поднимает тестовый сервер)
@@ -34,20 +40,38 @@ public class UsersControllersIntegrationTest {
 //        Assertions.assertEquals(2222, serverPort, "Порт не совпадает с ожидаемым!");
 //    }
 
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
     @Test
     @DisplayName("User can be created")
     void testCreateUser_whenValidDetailsProvided_returnsUserDetails() throws JSONException {
         // Arrange
         JSONObject userDetailsRequestJson = new JSONObject();
-        userDetailsRequestJson.put("firstName", "Sergey" );
-        userDetailsRequestJson.put("lastName", "Kargopolov" );
-        userDetailsRequestJson.put("email", "test3@test.com" );
-        userDetailsRequestJson.put("password", "12345678" );
-        userDetailsRequestJson.put("repeatPassword", "12345678" );
+        userDetailsRequestJson.put("firstName", "Sergey");
+        userDetailsRequestJson.put("lastName", "Kargopolov");
+        userDetailsRequestJson.put("email", "test3@test.com");
+        userDetailsRequestJson.put("password", "12345678");
+        userDetailsRequestJson.put("repeatPassword", "12345678");
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        httpHeaders.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+
+        HttpEntity<String> request = new HttpEntity<>(userDetailsRequestJson.toString(), httpHeaders);
 
         // Act
+        ResponseEntity<UserRest> response = testRestTemplate.postForEntity(
+                "/users",
+                request,
+                UserRest.class
+        );
+        UserRest  userRest = response.getBody();
 
         // Assert
+        Assertions.assertEquals(HttpStatus.OK.value(), response.getStatusCode().value());
+        Assertions.assertEquals(userDetailsRequestJson.getString("firstName"), userRest.getFirstName(), "firstName не совпадает!");
+
     }
 
 }
