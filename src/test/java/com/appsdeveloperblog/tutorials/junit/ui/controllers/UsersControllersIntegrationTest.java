d42c43a6410db89ceb63d8701b9fec9daa7bc712
parom <E.Parominskiy@isida.by>
2026-05-06 15:39:55 +0300

    94. @SpringBootTest: Defined Port Number

--- src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllersIntegrationTest.java
+++ src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllersIntegrationTest.java
@@ -1,12 +1,17 @@
 package com.appsdeveloperblog.tutorials.junit.ui.controllers;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 //@SpringBootTest // позволяет использовать все слои spring application
 //@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK) // используется по умолчанию (не поднимает тестовый сервер)
-
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT, // запускает embedded server
+        properties = {"server.port=9999"}) // override value from default application.properties
 public class UsersControllersIntegrationTest {
 
+    @Test
+    void contextLoads() {
 
+    }
 
 }
